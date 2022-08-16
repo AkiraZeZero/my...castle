9063f0e9270a9a8557ded214a207a32d8d56ed78
AkiraZeZero <kiara.ocasio07@gmail.com>
2022-08-16 12:57:19 -0400

    another one

--- Server/Controller/controller.js
+++ Server/Controller/controller.js
@@ -2,12 +2,12 @@ const path = require("path")
 
 module.exports = {
     getHomePage: (req, res) => {
-        res.sendFile(path.join(__dirname,`../client/index.html`))
+        res.sendFile(path.join(__dirname,`../../Client/index.html`))
     },
     getCSS: (req, res) => {
-        res.sendFile(path.join(__dirname, `../client/styles`))
+        res.sendFile(path.join(__dirname, `../../Client/style.css`))
     },
     getJavaScript: (req, res) => {
-        res.sendFile(path.join(__dirname, `../../client/index.html`))
+        res.sendFile(path.join(__dirname, `../../Client/main.js`))
     }
 }
