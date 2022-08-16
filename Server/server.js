7b3533ca5f8dfb079197533b40e2e3f40d9056c5
AkiraZeZero <kiara.ocasio07@gmail.com>
2022-08-16 13:04:36 -0400

    me again

--- Server/server.js
+++ Server/server.js
@@ -8,7 +8,7 @@ const { getHomePage, getCSS, getJavaScript } = require(`./Controller/controller`
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static("client"))
+app.use(express.static("Client"))
 
 // app.get('/', (req, res) => {
 //     res.sendFile(path.join(__dirname, `../client/index.html`))
